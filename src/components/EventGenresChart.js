824a4edf14e540599977a8258e5eee3eb6147c0b
crrenatacr <crrenatacr@gmail.com>
2025-01-06 14:46:27 +0100

    PieChartUpdate

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -1,3 +1,4 @@
+import { type } from "@testing-library/user-event/dist/cjs/utility/type.js";
 import React, { useState, useEffect } from "react";
 import {
   PieChart,
@@ -10,10 +11,9 @@ import {
 const EventGenresChart = ({ events }) => {
   const [data, setData] = useState([]);
   const genres = ["React", "JavaScript", "Node", "jQuery", "Angular"];
-  const colors = ["#8884d8", "#82ca9d", "#ffc658", "#ff8042", "#8dd1e1"];
+  const colors = ["#8884d8", "#82ca9d", "#ffc658", "#ff8042", "#8dd1e1"]; // Adicionei cores
 
-
-   const getData = () => {
+  const getData = () => {
     const data = genres.map((genre) => {
       const filteredEvents = events.filter((event) =>
         event.summary.includes(genre)
@@ -30,14 +30,14 @@ const EventGenresChart = ({ events }) => {
     setData(getData());
   }, [`${events}`]);
 
-  const renderCustomizedLabel = ({ 
-    cx, 
-    cy, 
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
     midAngle,
-     outerRadius,
-      percent,
-       index
-     }) => {
+    outerRadius,
+    percent,
+    index,
+  }) => {
     const RADIAN = Math.PI / 180;
     const radius = outerRadius;
     const x = cx + radius * Math.cos(-midAngle * RADIAN) * 1.07;
@@ -46,8 +46,8 @@ const EventGenresChart = ({ events }) => {
       <text
         x={x}
         y={y}
-        fill="#8884d8"
-        textAnchor={x > cx ? 'start' : 'end'}
+        fill={colors[index]}
+        textAnchor={x > cx ? "start" : "end"}
         dominantBaseline="central"
       >
         {`${genres[index]} ${(percent * 100).toFixed(0)}%`}
@@ -56,28 +56,29 @@ const EventGenresChart = ({ events }) => {
   };
 
   return (
-    <ResponsiveContainer width="99%" height={400}>
-      <PieChart>
+    <ResponsiveContainer width="99%" height={450}>
+      <PieChart 
+       margin={{ top: 60, bottom: 40 }}>
         <Pie
           data={data}
           dataKey="value"
           fill="#8884d8"
           labelLine={false}
           label={renderCustomizedLabel}
-          outerRadius={150}  
+          outerRadius={110}
         >
           {data.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={colors[index]} />
+            <Cell key={`cell-${index}`} fill={colors[index]} /> // Add colors to the chart slices
           ))}
         </Pie>
         <Legend
-					align="center"
-					verticalAlign="bottom"
-					layout="horizontal"
-					height={2}
-				/>
-      </PieChart>
-    </ResponsiveContainer>
+      align="center"
+      verticalAlign="bottom"
+      layout="horizontal"
+      height={40}
+      />
+  </PieChart>
+</ResponsiveContainer>
   );
 };
 
